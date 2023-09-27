fd53608d39d62c511d8c12a3602320cb9b64a1d7
Ivar Holmlund <ivholmlu@nmbu.no>
2023-09-27 20:40:39 +0200

    Model

--- sorting_data/model.py
+++ sorting_data/model.py
@@ -7,3 +7,29 @@ finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-ton
 tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
 
 df = pd.read_csv(r'..\No_sync\analyst_ratings_processed_filtered.csv')
+
+
+inputs = tokenizer(sentences, return_tensors="pt", padding=True)
+outputs = finbert(**inputs)[0]
+
+# Define a softmax function
+def softmax(x):
+    exp_x = np.exp(x - np.max(x))
+    return exp_x / exp_x.sum(axis=0)
+
+# Define labels for reference
+labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
+
+# Create an empty list to store data
+data = []
+
+for idx, sent in enumerate(sentences):
+    probabilities = softmax(outputs.detach().numpy()[idx])
+    row_data = [sent] + [f'{prob * 100:.2f}%' for prob in probabilities]
+    data.append(row_data)
+
+# Define column names for the DataFrame
+columns = ['title'] + [f'{label} Probability' for label in labels.values()]
+
+# Create a DataFrame
+new_df = pd.DataFrame(data, columns=columns)
