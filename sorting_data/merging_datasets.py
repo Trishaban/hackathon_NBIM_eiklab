056e30ee28c911039ca39980df0280537b9abd92
Ivar Holmlund <ivholmlu@nmbu.no>
2023-09-27 19:07:35 +0200

    Final push

--- sorting_data/merging_datasets.py
+++ sorting_data/merging_datasets.py
@@ -8,8 +8,5 @@ df_right = df_right[['Date', 'Adjusted Close']]
 #Merge on the common column of date
 df_merged = pd.merge(df_left, df_right, how="outer", left_on="converted_date", right_on="Date")
 
-print(df_merged.columns)
 df_merged = df_merged[["title", "Adjusted Close", "Date"]]
 df_merged.to_csv(r"data\date_price_tick.csv", index=False)
-print(len(df_merged["Date"].unique()))
-print(df_merged.head())
